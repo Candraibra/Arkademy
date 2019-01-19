7ad681590fbbd187a2200b75873fc88e36f1ec7d
candraibra <canisanie@gmail.com>
2019-01-19 10:57:56 +0700

    jos

--- soal_4.php
+++ soal_4.php
@@ -9,7 +9,11 @@
         var abjad = ["a", "b", "c", "d","e","f","g", "h", "i", "j","k","l",
         "m", "n", "o", "p","q","r","s", "t", "u", "v","w","x","y","z"];
         // mengambil radio
-        document.write(abjad[3,4]);
+        document.write(abjad[0]); document.write(abjad[1]); document.write(abjad[2]);
+        document.write('<br />');
+        document.write(abjad[3]); document.write(abjad[4]); document.write(abjad[5]);
+        document.write('<br />');
+        document.write(abjad[6]); document.write(abjad[7]); document.write(abjad[8]);
     </script>
 </body>
 </html>
